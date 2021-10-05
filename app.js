8fdbee03e5f98a6b4f1aeac2292be1bf639c32ea
Ankitv003 <ankitv78389@gmail.com>
2021-10-05 16:25:01 +0530

    some style edit

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var btnTranslate= document.querySelector("#btn-translate");
-var txtInput= document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL ="https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTransaltionURL(text){
-    return serverURL +"?" + "text="+ text
+function getTransaltionURL(text) {
+    return serverURL + "?" + "text=" + text
 }
 
-function errorHandler(error){
+function errorHandler(error) {
     console.log("error occured  ", error);
 }
 
-function clickHandler()
-{
-    var inputText= txtInput.value;  
+function clickHandler() {
+    var inputText = txtInput.value;
 
-fetch(getTransaltionURL(inputText))
-.then(response => response.json())
-.then(json=>{
-    var translatedText= json.contents.translated;
-    outputDiv.innerText= translatedText;})
-.catch(errorHandler)
+    fetch(getTransaltionURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 };
-btnTranslate.addEventListener("click",clickHandler)
+btnTranslate.addEventListener("click", clickHandler)
